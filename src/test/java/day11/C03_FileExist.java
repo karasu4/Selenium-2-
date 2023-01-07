7ebcc86c501396c4d9299d8600843b698f26c993
DavutKarasu <davut.karasu4@gmail.com>
2023-01-07 23:05:15 +0100

    07.01.2023

---
+++ src/test/java/day11/C03_FileExist.java
@@ -0,0 +1,34 @@
+package day11;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C03_FileExist {
+
+    @Test
+    public void isExistTest(){
+        String userDIR = System.getProperty("user.dir");
+        System.out.println(userDIR);///C:\Users\msi\AppData\Local\JetBrains\IdeaIC2022.2\log\indexing-diagnostic\com.B103Maven_Junit
+
+        String userHOME =  System.getProperty("user.home");
+        System.out.println(userHOME);///C:\Users\msi
+        //logo.jpeg indirip MASAUSTUNE kaydedin
+//        Logo masaustunde : /Users/msi/Desktop
+
+        String dosyaYolu = userHOME +"/Desktop/logo.jpeg";
+//        String dosyaYolu = userHOME +"\\Desktop\\logo.jpeg";
+        System.out.println(dosyaYolu);
+
+
+        boolean isExist = Files.exists(Paths.get(dosyaYolu));//doyda var ise tru, yok ise false
+        Assert.assertTrue(isExist);
+        /*
+        1. fail -> PATH YANLIS : dosya masa ustunde degil, dosya adi yanlis, yada arada onedrive gibi ex dosyalar olarak
+         */
+
+
+    }
+}
