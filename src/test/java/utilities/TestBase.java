6c9d4b83950bf78cbb417f5e39c38b25152895e6
DavutKarasu <davut.karasu4@gmail.com>
2023-01-07 19:09:19 +0100

    06.01.2023

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,33 +1,40 @@
 package utilities;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+
 public abstract class TestBase {
-    //    TestBase i abstract yapmamizin sebebi bu sinifin objesini olusturmak istemiyorum
+//    TestBase i abstract yapmamizin sebebi bu sinifin objesini olusturmak istemiyorum
 //    TestBase testBase = new TestBase(); -> YAPILAMAZ
 //    Amacim bu sinifi extend etmek ve icindeki hazir metodlari kullanmak
-//    driver objesini olustur. Driver ya public yada protected olmali.
+
+    //    driver objesini olustur. Driver ya public yada protected olmali.
 //    Sebepi child classlarda gorulebilir olmasi
     protected static WebDriver driver;
+
     //    setUp
     @Before
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-       // driver=WebDriverManager.chromedriver().create();
+//        driver=WebDriverManager.chromedriver().create();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
     //    tearDown
     @After
     public void tearDown(){
+        waitFor(5);
         driver.quit();
     }
+
     //    MULTIPLE WINDOW:
 //    1 parametre alir : Gecis Yapmak Istedigim sayfanin Title
 //    ORNEK:
@@ -44,6 +51,8 @@ public abstract class TestBase {
         }
         driver.switchTo().window(origin);
     }
+
+
     //    windowNumber sıfır (0)'dan başlıyor.
 //    index numarasini parametre olarak alir
 //    ve o indexli pencerece gecis yapar
@@ -51,6 +60,9 @@ public abstract class TestBase {
         List<String> list = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(list.get(windowNumber));
     }
+
+
+
     /*   HARD WAIT:
      @param : second
     */
@@ -61,4 +73,5 @@ public abstract class TestBase {
             e.printStackTrace();
         }
     }
+
 }
