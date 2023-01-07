2e9ac4bb41605ea6da3bc4ad6f1fef62844de410
DavutKarasu <davut.karasu4@gmail.com>
2023-01-07 21:04:41 +0100

    07.01.2023

--- src/test/java/day11/C01_Actions5.java
+++ src/test/java/day11/C01_Actions5.java
@@ -51,5 +51,15 @@ public class C01_Actions5 extends TestBase {
                 //     2.actions objesi ile kullanacağımız web elementi locate ederiz...
                 //     3.istediğimiz methodları action objesi ile kullanırız.
                 //     actions.doubleClick(element ismi).perform;
+
+        //////////////////////////////////////////////////////////////////////////
+        /*
+        1- Actions nedir? Actions Class nedir ?
+*Actions selenium dan gelen bir kütüphanedir. Mouse ve Keyboard islemlerini gerçekleştirmek icin kullandigimiz hazır bir selenium kutuphanesidir. Ornegin, sag tiklamak(contextClick), çift tıklamak(doubleClick), elementin uzerine gitmek(moveToElement) gibi mouse islemlerini gerçekleştirebiliriz. Ayni zamanda, shift, control, delete, enter gibi keyboard tuslarinada actions yardımıyla basabiliriz. Bu tur islemlerde sendKeys metodu kullaninir.
+2- Ne tur methodlar kullandin?
+Actions metotlari onemlidir. Ve tekrar tekrar kullanmam gerektiğinden, Actions metotlarını içeren Reusable metodları oluşturdum. Ve gerektiğinde bu reusable metodları kullanıyorum. Ornegin rightClick methodunu bir elemente raga tiklamak icin test classimda vagira bilirim.
+3- Hangi methodlari kullandin ne ne ise yarar?
+Ornegin, sag tiklamak(contextClick), çift tıklamak(doubleClick), elementin uzerine gitmek(moveToElement) gibi mouse islemlerini gerçekleştirebiliriz. Ayni zamanda, shift, control, delete, enter gibi keyboard tuslarinada actions yardımıyla basabiliriz. Bu tur islemlerde sendKeys metodu kullaninir.
+         */
     }
 }
