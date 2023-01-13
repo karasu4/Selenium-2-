e24b14a6e47689fe718e6ceefc2b86168560d40d
DavutKarasu <davut.karasu4@gmail.com>
2023-01-13 21:35:39 +0100

    13.01.2023

---
+++ src/test/java/day14/C03_ExtentReports.java
@@ -0,0 +1,17 @@
+package day14;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.junit.Test;
+import utilities.TestBase;
+public class C03_ExtentReports extends TestBase {
+    //    HATIRLAMAMIZ GEREKEN 2 DEGISKEN
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
+
+    @Test
+    public void extentReportsTest(){
+//
+    }
+}
